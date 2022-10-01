41e0103a58f3ea762293685dc171d0d5d4db9c0f
Udit-Gogia <gogiaudit17@gmail.com>
2022-10-01 15:16:59 +0530

    logos

---
+++ app.js
@@ -0,0 +1,10 @@
+var data = {
+	labels: [1, 2, 3, 4, 5, 6, 7, 8],
+	series: [[5, 9, 7, 8, 5, 3, 5, 4]],
+};
+
+var options = {
+	low: 0,
+	showArea: true,
+};
+new Chartist.Line(".ct-chart", data, options);
